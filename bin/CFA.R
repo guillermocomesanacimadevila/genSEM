2cf678c0f17cb32a84f8664268d4ef4f2b7afd41
Guillermo Comesana Cimadevila <comesanaguillermo@gmail.com>
2026-05-12 11:29:29 +0100

    add CFA scr for 3 GWAS

---
+++ bin/CFA.R
@@ -0,0 +1,68 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library(GenomicSEM)
+})
+
+args <- commandArgs(trailingOnly=TRUE)
+
+if (length(args) < 4) {
+  stop("Usage: Rscript CFA.R <pheno1_id> <pheno2_id> <pheno3_id> <out_dir>")
+}
+
+pheno1_id <- args[1]
+pheno2_id <- args[2]
+pheno3_id <- args[3]
+out_dir <- args[4]
+
+trait_names <- c(
+  pheno1_id,
+  pheno2_id,
+  pheno3_id
+)
+
+hm3 <- "ref/ldsc/eur_w_ld_chr/w_hm3.snplist"
+ld <- "ref/ldsc/eur_w_ld_chr/"
+wld <- "ref/ldsc/weights_hm3_no_hla/"
+
+# LOAD LDSC output
+ldsc <- file.path(out_dir, "ldsc", "LDSC_INT.rds")
+LDSC_INT <- readRDS(ldsc)
+
+
+# exploratory factor analysis (EFA) # model 3 
+# Load AD, MDD and SCZ into a single latent factor 
+# F1 =~ AD + SCZ + MDD
+
+# contruct multiv LDSC matrix
+# CFA
+covstruc <- LDSC_INT
+INT.model <- paste0("F1 =~ ", paste(trait_names, collapse=" + "))
+
+IntResults <- usermodel(
+  covstruc=covstruc,
+  model=INT.model,
+  std.lv=TRUE
+)
+
+
+sem_dir <- file.path(out_dir, "sem")
+dir.create(sem_dir, recursive=TRUE, showWarnings=FALSE)
+
+sink(file.path(sem_dir, "CFA_console_output.txt"))
+
+IntResults <- usermodel(
+  covstruc=covstruc,
+  model=INT.model,
+  std.lv=TRUE
+)
+
+sink()
+
+if (!is.null(IntResults)) {
+  saveRDS(IntResults, file=file.path(sem_dir, "CFA_results.rds"))
+  write.table(IntResults, file=file.path(sem_dir, "CFA_results.tsv"), sep="\t", quote=FALSE, row.names=FALSE)
+}
+
+print("Model fit")
+IntResults$modelfit
